79b7b3805e719f61bdeeec5d126f93b65cd1a121
vitormanfre <vitor.manfre@tegra.me>
2018-08-15 16:37:44 -0300

    adds source code

---
+++ sawtooth/voteHandler.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { TransactionHandler } = require('sawtooth-sdk/processor/handler')
+const { Decoder } = require('cbor')
+const { calculateVoteAddress, handlerInfo } = require('./infra');
+
+const encode = obj => Buffer.from(JSON.stringify(obj, Object.keys(obj).sort()))
+
+class VoteHandler extends TransactionHandler {
+  constructor() {
+    console.log('Iniciando smart contract para votos ')
+    const info = handlerInfo();
+    super(info.family, [info.version], [info.prefix]);
+  }
+
+  apply(txn, context) {
+    console.log("chegando uma nova transacao");
+
+    const dataDecoded = Decoder.decodeFirstSync(txn.payload);
+    const payload = JSON.parse(dataDecoded);
+
+    const blockAddress = calculateVoteAddress(payload)
+    const { candidateNumber, ellectionName } = payload;
+
+    return context.setState({
+      [blockAddress]: encode({ candidateNumber, ellectionName })
+    });
+  }
+}
+
+module.exports = {
+  VoteHandler
+}
